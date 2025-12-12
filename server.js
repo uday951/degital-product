7986b7f45e646afcda6c50cec7fd8ccc65ae0bf8
Uday915 <udaydd6062@gmail.com>
2025-12-12 19:26:07 +0530

    added

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ mongoose.connect(process.env.MONGODB_URI)
     console.log('✅ Connected to MongoDB');
     
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
